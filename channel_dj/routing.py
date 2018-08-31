43a001093c024d3003c6b4a4506f6228c73ef52c
alexHooper <alexmohack@gmail.com>
2018-08-31 21:07:59 +0530

    create routes for connect and disconnect functions

--- channel_dj/routing.py
+++ channel_dj/routing.py
@@ -1,1 +1,8 @@
 from channels.routing import route
+from display.consumers import ws_connect, ws_disconnect
+
+
+channel_routing = [
+	route('websocket.connect', ws_connect),
+	route('websocket.disconnect', ws_disconnect)
+]
